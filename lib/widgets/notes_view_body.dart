88f61e6d9b927621ef0efae386ccd698fd6499ed
EslamSaeed570 <solomelprens12345@gmail.com>
2025-05-05 23:39:19 +0300

    create List view

---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/widgets/notes_list_view.dart';
+
+import 'custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 22, vertical: 5),
+      child: Column(
+        children: [
+          SizedBox(
+           
+            height: 20,
+          ),
+          CustomAppBar(),
+          SizedBox(
+            height: 14,
+          ),
+          Expanded(child: NotesListView())
+        ],
+      ),
+    );
+  }
+}
