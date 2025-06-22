775806620b7fd9819880a39a3a59f17bcb98e299
EslamSaeed570 <solomelprens12345@gmail.com>
2025-06-22 12:47:27 +0300

    change model progress hud to loading indicator in add note button sheet

--- lib/views/widgets/add_note_bottem_sheet.dart
+++ lib/views/widgets/add_note_bottem_sheet.dart
@@ -13,7 +13,7 @@ class AddNoteBottemSheet extends StatelessWidget {
     return BlocProvider(
       create: (context) => AddNoteCubit(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.all(16),
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
           listener: (context, state) {
             if (state is AddNoteSuccess) {
@@ -24,8 +24,8 @@ class AddNoteBottemSheet extends StatelessWidget {
             }
           },
           builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoading ? true : false,
+            return AbsorbPointer(
+              absorbing: state is AddNoteLoading ? true : false,
               child: const SingleChildScrollView(
                 child: AddNoteForm(),
               ),
--- lib/views/widgets/custom_button_sheet.dart
+++ lib/views/widgets/custom_button_sheet.dart
@@ -2,23 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/views/widgets/cosnstants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
- final void Function()? onTap;
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
+  final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:onTap ,
+      onTap: onTap,
       child: Container(
         width: MediaQuery.of(context).size.width,
         height: 40,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8), color: kPrimaryColor),
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-                color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),
-          ),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                  height: 25,
+                  width: 25,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),
+                ),
         ),
       ),
     );
